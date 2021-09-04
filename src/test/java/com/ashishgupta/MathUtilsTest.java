6663545328d7c97ea04f06116554349a5bea0111
Ashish2796 <ashishgupta2796@gmail.com>
2021-09-04 19:44:24 +0530

    DisplayName,Disabled,Enable*,AssumeThat(assume*) Annotataion or handling execution

--- src/test/java/com/ashishgupta/MathUtilsTest.java
+++ src/test/java/com/ashishgupta/MathUtilsTest.java
@@ -1,8 +1,13 @@
 package com.ashishgupta;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 class MathUtilsTest {
 
@@ -30,4 +35,43 @@ class MathUtilsTest {
 		MathUtils obj = new MathUtils();
 		assertThrows(ArithmeticException.class,()->{obj.divide(1, 0);} ,"Divide by zero");
 	}
+	
+	@Test
+	@Disabled
+	void testDisabled() {
+		//fail("Not yet implemented");
+		System.out.println("Test disabled.");
+	}
+	
+	
+	/**
+	 *  test execute only on linux os
+	 *  similarly we can execute test based on jre, env prop, variable, OS etc
+	 */
+	@Test
+	@EnabledOnOs(OS.LINUX)
+	void testEnable() {
+		//fail("Not yet implemented");
+		System.out.println("Test disabled.");
+	}
+	
+	/**
+	 *  based on assumption test execution will execute
+	 *  
+	 */
+	@Test
+	@DisplayName("Based on Assumpion test case will execute")
+	void testAssumeThat() {
+		//fail("Not yet implemented");
+		boolean isServerup= true;
+		
+		assumeTrue(isServerup);
+		MathUtils obj = new MathUtils();
+		assertThrows(ArithmeticException.class,()->{obj.divide(1, 0);} ,"Divide by zero");
+	}
+	
+	
+	
+	
+	
 }
